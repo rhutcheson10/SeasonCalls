2a8522fb1b8e3b84b453b947553eb3edf48e729d
rhutcheson10 <rhutcheson1@gmail.com>
2019-06-04 15:00:32 +1200

    2 files modified, 1 added, 1 deleted

---
+++ src/app/callproducts.ts
@@ -0,0 +1,15 @@
+export const callproducts = [
+  {
+    name: 'Season GH2',
+    price: 180,
+    colour: 'Black',
+    description: 'The GH2 Fist Seasons Call in Flat Smoke or flat black'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { callproducts} from '../callproducts';
 
 @Component({
   selector: 'app-product-list',
@@ -8,7 +8,7 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  callproducts = callproducts;
 
   share() {
     window.alert('The product has been shared!');
